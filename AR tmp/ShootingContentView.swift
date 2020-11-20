a812e58bb13e8eada8ec20063a20bd448fc73cdc
Elias Lankinen <elias@lankinen.xyz>
2020-11-20 17:07:39 +0200

    right direction (maybe?)

--- AR tmp/ShootingContentView.swift
+++ AR tmp/ShootingContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import RealityKit
+import ARKit
 
 struct ShootingContentView : View {
     @State var abc: Bool = false
@@ -26,16 +27,12 @@ extension ARView {
     
     @objc func handleTap(_ sender: UITapGestureRecognizer? = nil) {
         print("tap")
-        self.shootBox()
-//        if let anchor = self.scene.anchors.first, let child = anchor.children.first {
-//            print("child: \(child)")
-//            let entity = child as! (Entity & HasPhysics)
-//            entity.addForce([100,100,100], relativeTo: anchor)
-//        }
+        
+        let anchor = ARAnchor(name: "Particle", transform: self.cameraTransform.matrix)
+        self.session.add(anchor: anchor)
     }
     
     func shootBox() {
-        let cameraTransform = self.cameraTransform
         let box = ModelEntity(mesh: .generateBox(size: 5), materials: [gold])
         
         box.components.set(CollisionComponent(
@@ -46,39 +43,9 @@ extension ARView {
                 
         box.components.set(PhysicsBodyComponent(massProperties: .default, material: .default, mode: .dynamic))
         
-        let x: Float = cameraTransform.rotation.imag.x
-        let y: Float = cameraTransform.rotation.imag.y
-        let z: Float = cameraTransform.rotation.imag.z
-        print("x: \(x) y: \(y) z: \(z)")
-        var nz: Float = y
-        if nz > 0.5 {
-            nz = 2 * (y - 0.5)
-        } else if nz > 0 {
-            nz = 2 * (y - 0.5)
-        } else if nz < -0.5 {
-            nz = -2 * (y + 0.5)
-        } else if nz < 0 {
-            nz = -2 * (y + 0.5)
-        }
-        
-        var nx: Float = 0
-        if y > 0 {
-            nx = (1 - abs(nz)) * -1
-        } else {
-            nx = 1 - abs(nz)
-        }
-        print("nx: \(nx)")
-        print("nz: \(nz)")
-        let velX = nx * 50
-        let velZ = nz * 50
-        print("velX: \(velX)")
-        print("velZ: \(velZ)")
-        
-//        box.components.set(PhysicsMotionComponent(linearVelocity: [velX,0,velZ]))
-        box.components.set(PhysicsMotionComponent(linearVelocity: [0,0,-10]))
+        box.components.set(PhysicsMotionComponent(linearVelocity: [0,0,-50]))
         
         let lcam = AnchorEntity(world: self.cameraTransform.matrix)
-//        let lcam = AnchorEntity(transform: self.cameraTransform.matrix)
         self.scene.addAnchor(lcam)
         lcam.addChild(box)
     }
@@ -87,9 +54,40 @@ extension ARView {
 struct ARViewContainer: UIViewRepresentable {
     @Binding var abc: Bool
     
+    class Coordinator: NSObject, ARSessionDelegate {
+        func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+            print("session")
+            for anchor in anchors {
+                if let anchorName = anchor.name, anchorName == "Particle" {
+                    print("anchorName")
+                    let box = ModelEntity(mesh: .generateBox(size: 5), materials: [gold])
+                    
+                    box.components.set(CollisionComponent(
+                        shapes: [.generateBox(size: [5, 5, 5])],
+                        mode: .default,
+                        filter: .default
+                    ))
+                            
+                    box.components.set(PhysicsBodyComponent(massProperties: .default, material: .default, mode: .dynamic))
+                    
+                    box.components.set(PhysicsMotionComponent(linearVelocity: [0,0,-10]))
+//                    let lcam = AnchorEntity(anchor: anchor)
+//                    lcam.addChild(box)
+//                    session.add(anchor: lcam)
+                }
+            }
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
+    
     func makeUIView(context: Context) -> ARView {
         let arView = ARView(frame: .zero)
         arView.debugOptions = [.showPhysics]
+        
+        arView.session.delegate = context.coordinator
         
         arView.setupGestures()
         arView.shootBox()
